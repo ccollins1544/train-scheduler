f021cf2643b1db91efa176c0cace79c5cd53828a
Christopher Collins <chris@ccollins.io>
2019-09-30 00:57:25 -0600

    Updated addTrainToSchedule. Created updateTrainSchedule to run every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,17 +13,17 @@
  * 
  * 1. Functions
  *   1.1 firebaseWatcher
- *   1.2 updateTrainSchedule
+ *   1.2 addTrainToSchedule
  *   1.3 addTrain
  *   1.4 trainMinutesAway
+ *   1.5 updateTrainSchedule
  * 
  * 2. Document Ready
  *   2.1 Watch Database + Initial Loader
  *   2.2 Add Train on Submit
+ *   2.3 Update Train Schedule Every minute
  * 
  * @todo
- * -Calculate 'next-arrival' and 'minutes-away' from 'train-frequncy' and 'train-start-time'.
- * 
  * -Update 'next-arrival' and 'minutes-away' every minute. 
  * -Add [update] and [remove] buttons for each train. Updating should allow changing: name, destination, and arrival time (where arrival time is relation to first-train-time).
  * -Make so only users that log into the site with their google or github accounts can use your site. Check out Firebase authentication.
@@ -44,7 +44,7 @@ var firebaseConfig = {
 };
 
 // 0.2 Initialize Firebase
-firebase.initializeApp(firebaseConfig); 
+firebase.initializeApp(firebaseConfig);
 var fdb = firebase.database();
 var dbRef = fdb.ref("/trainScheduler");
 
@@ -55,29 +55,29 @@ var scheduleTableFields = ["train-name", "train-destination", "train-frequency",
 /**
  * 1.1 firebaseWatcher
  * Watches firebase database for either a 'value' or 'child_added' and fetches fields that match scheduleTableFields 
- * array and saves them to tableData. Then finally calls updateTrainSchedule(tableData) when event occurs.
+ * array and saves them to tableData. Then finally calls addTrainToSchedule(tableData) when event occurs.
  * 
  * @param {firebase.database().ref("/child")} databaseReference 
  * @param {string} onChange - can be 'value' or 'child_added'
  */
-var firebaseWatcher = function(databaseReference, onChange="child_added"){
+var firebaseWatcher = function (databaseReference, onChange = "child_added") {
   // set default database reference to our global dbRef
   databaseReference = (databaseReference === undefined) ? dbRef : databaseReference;
 
   // Firebase watcher + initial loader 
-  databaseReference.on(onChange, function(snap){
+  databaseReference.on(onChange, function (snap) {
     // console.log(snap.val());
-    
+
     var tableData = {};
     var firstTime = false;
-    for (var property in snap.val()){
-      if(snap.val().hasOwnProperty(property)){
+    for (var property in snap.val()) {
+      if (snap.val().hasOwnProperty(property)) {
         // console.log(property + " : " + snap.val()[property]);
 
-        if(scheduleTableFields.includes(property)){
+        if (scheduleTableFields.includes(property)) {
           tableData[property] = snap.val()[property];
 
-        }else if(property === "train-start-time"){
+        } else if (property === "train-start-time") {
           firstTime = snap.val()[property];
         }
 
@@ -85,7 +85,7 @@ var firebaseWatcher = function(databaseReference, onChange="child_added"){
     }
 
     // Calculate 'next-arrival' time and 'minutes-away' 
-    if(firstTime !== false && tableData.hasOwnProperty('train-frequency')) {
+    if (firstTime !== false && tableData.hasOwnProperty('train-frequency')) {
       var minutesAway = trainMinutesAway(firstTime, tableData["train-frequency"]);
       var nextTrain = moment().add(minutesAway, "minutes");
 
@@ -94,26 +94,26 @@ var firebaseWatcher = function(databaseReference, onChange="child_added"){
     }
 
     console.log("TableData", tableData);
-    updateTrainSchedule(tableData);
+    addTrainToSchedule(tableData);
 
-  }, function (errorObject){
+  }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
 
 } // END firebaseWatcher
 
 /**
- * 1.2 updateTrainSchedule
+ * 1.2 addTrainToSchedule
  * Adds a row to #train-schedule table body. 
  * @param {object} tableRowObj - properties in this object must be inside the global scheduleTableFields
  */
-var updateTrainSchedule = function(tableRowObj){
+var addTrainToSchedule = function (tableRowObj) {
   var newRow = $("<tr>");
 
-  for (var i in scheduleTableFields){
+  for (var i in scheduleTableFields) {
     var KEY = scheduleTableFields[i];
 
-    if(tableRowObj.hasOwnProperty(KEY)){
+    if (tableRowObj.hasOwnProperty(KEY)) {
       var VALUE = tableRowObj[KEY];
       newRow.append(
         $("<td>").text(VALUE)
@@ -123,30 +123,30 @@ var updateTrainSchedule = function(tableRowObj){
   } // loop through scheduleTableFields
 
   $("#train-schedule > tbody").append(newRow);
-} // END updateTrainSchedule
+} // END addTrainToSchedule
 
 /**
  * 1.3 addTrain
  * Gets form data from #add-train-form adds it to the database.
  * @param {object} event 
  */
-function addTrain(event){
+function addTrain(event) {
   event.preventDefault();
 
   var formArray = $("#add-train-form").serializeArray();
   var formData = {}; // NOTE: {} creates and object and [] creates an Array.
 
-  for (var i in formArray){
+  for (var i in formArray) {
     var KEY = "";
     var VALUE = "";
 
-    for(var key in formArray[i]){
+    for (var key in formArray[i]) {
       // console.log(key+" => "+formArray[i][key]);
 
-      if(key == "name"){
+      if (key == "name") {
         KEY = formArray[i][key];
 
-      }else if(key == "value"){
+      } else if (key == "value") {
         VALUE = formArray[i][key];
       }
 
@@ -158,7 +158,7 @@ function addTrain(event){
   dbRef.push(formData);
 
   // Clear Form Values
-  $("#add-train-form *").filter(":input").each(function(){
+  $("#add-train-form *").filter(":input").each(function () {
     $(this).val("");
   });
 
@@ -170,33 +170,84 @@ function addTrain(event){
  * @param {integer} minFrequency - 
  * @return {integer} minutesAway - time in minutes until next train. 
  */
-var trainMinutesAway = function(firstTime, minFrequency) {
-  if(firstTime === undefined || minFrequency === undefined ){ return; }
-  
+var trainMinutesAway = function (firstTime, minFrequency) {
+  if (firstTime === undefined || minFrequency === undefined) {
+    return;
+  }
+
   // First Time (pushed back 1 year to make sure it comes before current time)
   var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-  
+
   // Difference between the times
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  
+
   // Time apart (remainder)
   var tRemainder = diffTime % minFrequency;
-  
+
   // Minute Until Train
   var minutesAway = minFrequency - tRemainder;
 
   return minutesAway;
 } // END trainMinutesAway
 
+/**
+ * 1.5 updateTrainSchedule
+ */
+var updateTrainSchedule = function () {
+  dbRef.once('value', (snapshot) => {
+  
+    if (snapshot.numChildren() === 0) { return; }
+    
+    $("#train-schedule > tbody").empty();
+    snapshot.forEach(function (snap) {
+
+      var tableData = {};
+      var firstTime = false;
+      for (var property in snap.val()) {
+        if (snap.val().hasOwnProperty(property)) {
+          // console.log(property + " : " + snap.val()[property]);
+
+          if (scheduleTableFields.includes(property)) {
+            tableData[property] = snap.val()[property];
+
+          } else if (property === "train-start-time") {
+            firstTime = snap.val()[property];
+          }
+        }
+      }
+
+      // Calculate 'next-arrival' time and 'minutes-away' 
+      if (firstTime !== false && tableData.hasOwnProperty('train-frequency')) {
+        var minutesAway = trainMinutesAway(firstTime, tableData["train-frequency"]);
+        var nextTrain = moment().add(minutesAway, "minutes");
+
+        tableData['next-arrival'] = moment(nextTrain).format("HH:mm");
+        tableData['minutes-away'] = minutesAway;
+      }
+
+      console.log("TableData", tableData);
+      addTrainToSchedule(tableData);
+
+    }); // END snapshot.forEach(function(snap)
+
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+
+  }); // END dbRef.once('value', (snapshot) => {
+}; // END updateTrainSchedule
+
 /**===============[ 2. Document Ready ]==================== 
  * NOTE: $(function(){ === $(document).ready(function() {
  * it's the shorthand version of document ready. 
  *********************************************************/
-$(function(){
+$(function () {
   // 2.1 Watch Database + Initial Loader
   firebaseWatcher();
 
   // 2.2 Add Train on Submit
   $('#add-train-form').submit(addTrain);
-  
+
+  // 2.3 Update Train Schedule Every minute
+  var intervalID = setInterval(updateTrainSchedule, 60 * 1000);
+
 }); // END document ready
