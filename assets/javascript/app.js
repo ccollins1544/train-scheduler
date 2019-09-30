ab25ac78429d27eaf8f431e2a3e6a21fba4b979c
Christopher Collins <chris@ccollins.io>
2019-09-29 21:46:25 -0600

    Created updateTrainSchedule and addTrain functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,13 @@
  * ===============[ TABLE OF CONTENTS ]===================
  * 0. Globals
  *   0.1 Firebase Configuration
+ *   0.2 Initialize Firebase
+ *   0.3 scheduleTableFields
  * 
  * 1. Functions
  *   1.1 firebaseWatcher
+ *   1.2 updateTrainSchedule
+ *   1.3 addTrain
  * 
  * 2. Document Ready
  *   2.1 Watch Database + Initial Loader
@@ -32,10 +36,13 @@ var firebaseConfig = {
   appId: "1:541445299555:web:d9ceca0430a78546108756"
 };
 
-// Initialize Firebase
+// 0.2 Initialize Firebase
 firebase.initializeApp(firebaseConfig); 
 var fdb = firebase.database();
 var dbRef = fdb.ref("/trainScheduler");
+
+// 0.3 scheduleTableFields 
+var scheduleTableFields = ["train-name", "train-destination", "train-frequency", "next-arrival", "minutes-away"];
 
 /* ===============[ 1. Functions ]=======================*/
 /**
@@ -43,13 +50,31 @@ var dbRef = fdb.ref("/trainScheduler");
  * @param {*} databaseReference 
  * @param {*} onChange - can be 'value' or 'child_added'
  */
-var firebaseWatcher = function(databaseReference, onChange="value"){
+var firebaseWatcher = function(databaseReference, onChange="child_added"){
   // set default database reference to our global dbRef
   databaseReference = (databaseReference === undefined) ? dbRef : databaseReference;
 
   // Firebase watcher + initial loader 
   databaseReference.on(onChange, function(snap){
-    console.log(snap.val());
+    // console.log(snap.val());
+    
+    var tableData = {};
+    for (var property in snap.val()){
+      if(snap.val().hasOwnProperty(property)){
+        // console.log(property + " : " + snap.val()[property]);
+
+        if(scheduleTableFields.includes(property)){
+          tableData[property] = snap.val()[property];
+        }
+
+      }
+    }
+
+    tableData['next-arrival'] = "7:00";
+    tableData['minutes-away'] = "7";
+
+    console.log(tableData);
+    updateTrainSchedule(tableData);
 
   }, function (errorObject){
     console.log("The read failed: " + errorObject.code);
@@ -58,7 +83,29 @@ var firebaseWatcher = function(databaseReference, onChange="value"){
 } // END firebaseWatcher
 
 /**
- * addTrain
+ * 1.2 updateTrainSchedule
+ * @param {object} tableRowObj - properties in this object must be inside the global scheduleTableFields
+ */
+var updateTrainSchedule = function(tableRowObj){
+  var newRow = $("<tr>");
+
+  for (var i in scheduleTableFields){
+    var KEY = scheduleTableFields[i];
+
+    if(tableRowObj.hasOwnProperty(KEY)){
+      var VALUE = tableRowObj[KEY];
+      newRow.append(
+        $("<td>").text(VALUE)
+      );
+    }
+
+  } // loop through scheduleTableFields
+
+  $("#train-schedule > tbody").append(newRow);
+} // END updateTrainSchedule
+
+/**
+ * 1.3 addTrain
  * Gets form data from #add-train-form adds it to the database
  * @param {object} event 
  */
@@ -73,7 +120,7 @@ function addTrain(event){
     var VALUE = "";
 
     for(var key in formArray[i]){
-      console.log(key+" => "+formArray[i][key]);
+      // console.log(key+" => "+formArray[i][key]);
 
       if(key == "name"){
         KEY = formArray[i][key];
@@ -83,12 +130,17 @@ function addTrain(event){
       }
 
     }
-
     formData[KEY] = VALUE;
   }
 
-  console.log(formData);
-  
+  formData.dateAdded = firebase.database.ServerValue.TIMESTAMP;
+  dbRef.push(formData);
+
+  // Clear Form Values
+  $("#add-train-form *").filter(":input").each(function(){
+    $(this).val("");
+  });
+
 } // addTrain
 
 /**===============[ 2. Document Ready ]==================== 
@@ -96,12 +148,8 @@ function addTrain(event){
  * it's the shorthand version of document ready. 
  *********************************************************/
 $(function(){
-
   // 2.1 Watch Database + Initial Loader
   firebaseWatcher();
-
-  // Debugging
-  firebaseWatcher(fdb.ref("/bidderData"));
 
   // 2.2 Add Train on Submit
   $('#add-train-form').submit(addTrain);
